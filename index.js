89e99c86b175aafd192ca2b7b1743b02a7b3cc44
Arpit Mallick <arpit@Arpits-Air.lan>
2023-10-04 00:38:38 -0700

    Added sounds to buttons

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-document.querySelector("body button").addEventListener("click",clickButton);
-
-function clickButton(){
-    alert("Hello");
-};
+for(var i=0;i<document.querySelectorAll("body button").length;i++){
+    document.querySelectorAll("body button")[i].addEventListener("click",function(){
+        var clickInnerHtml = this.innerHTML;
+        switch(clickInnerHtml){
+            case "w":
+                var crash = new Audio("./sounds/crash.mp3");
+                crash.play();
+                break;
+            case "a":
+                var kick = new Audio("./sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            case "s":
+                var snare = new Audio("./sounds/snare.mp3");
+                snare.play();
+                break;
+            case "d":
+                var tom1 = new Audio("./sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "j":
+                var tom2 = new Audio("./sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "k":
+                var tom3 = new Audio("./sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "l":
+                var tom4 = new Audio("./sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            default: console.log(clickInnerHtml);
+        }
+    });
+}
 
